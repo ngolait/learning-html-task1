61d79225fe78e222a022df42f2afc858ad423016
José da Silva <josesilva23p@gmai..com>
2020-12-04 13:39:58 +0100

    configure babel whith webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
     filename: 'todo.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+
 };
